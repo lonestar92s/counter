8f86e5e11c6b82a862f4d6f7c19380b8d04588e3
Andrew Aluko <aluko17@icloud.com>
2019-03-26 13:17:37 -0500

    subtract

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 export default class App extends Component {
- 
-handleClick = (event) =>{
+
+addition = (event) =>{
 this.setState({
   counter: this.state.counter + 1
-})
+  })
+}
+
+subtraction = (event) => {
+  this.setState({
+    counter: this.state.counter - 1
+  })
 }
  state = {
   counter: 0
- }
+}
+
+
 
   render() {
     return (
       <div>
       <h1>{this.state.counter}</h1>
-      <button onClick={this.handleClick}>Add</button>
+      <button onClick={this.addition}>Add</button>
+      <button onClick={this.subtraction}>Subtract</button>
       </div>
     )
   }
