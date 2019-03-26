3ce7ac06eb7bf41a0478c8043a019b2675bc35d0
Andrew Aluko <aluko17@icloud.com>
2019-03-26 13:00:22 -0500

    addition

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 export default class App extends Component {
+ 
+handleClick = (event) =>{
+this.setState({
+  counter: this.state.counter + 1
+})
+}
+ state = {
+  counter: 0
+ }
+
   render() {
     return (
       <div>
-      <h1>hello</h1>
+      <h1>{this.state.counter}</h1>
+      <button onClick={this.handleClick}>Add</button>
       </div>
     )
   }
